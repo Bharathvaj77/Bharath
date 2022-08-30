376fd7890af965795052012adf03e40eef3a857f
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-08-30 09:16:52 +0530

    comparitive operator

--- src/Bharath/Vaj.java
+++ src/Bharath/Vaj.java
@@ -6,17 +6,8 @@ public class Vaj {
 
 	
 	public static void main(String[] args) {
-		int x = 5;
-		int y = 3;
-		int z = 7;
-		x = x+y+z;
-		y = x-y-z;
-		z = x-y-z;
-		x= x-y-z;
-		
-		System.out.println(z);
-		System.out.println(x);
-		System.out.println(y);
+		int age = 15;
+		System.out.println(age>=18);
 		
 	}
 
