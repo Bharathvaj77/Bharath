fb9cba8cb65f4f3bf205889fbeba4f67d4a01a51
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-07 09:01:19 +0530

    reverse the digit

--- src/Bharath/Vaj.java
+++ src/Bharath/Vaj.java
@@ -6,13 +6,16 @@ public class Vaj {
 
 	
 	public static void main(String[] args) {
-		int num = 9876 ,sum=0;
+		int num = 9876 ;
+		int reverse=0;
 		while (num>0) {
 			int r = num%10;
-			sum = sum+r;
-			System.out.println(sum);
+			reverse = reverse*10+r;
+			
 			num = num/10;
 		}
+			
+		System.out.println( reverse);
 		
 		
 	}
