d92955c5ebebaa6f2c474598370af1b58f0a1ec1
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-09 19:34:26 +0530

    prime || not prime

--- src/Bharath/BT.java
+++ src/Bharath/BT.java
@@ -3,12 +3,26 @@ package Bharath;
 public class BT {
 
 	public static void main(String[] args) {
-		int sum =1;
-		for(int i=1;i<=5;i++) {
-			sum = sum*i;
+		int n = 15;
+		int count = 0;
+		int i = 2;
+		boolean flag = false;
+		while(i<=n/2) {
+			if (n%i==0) {
+				count++;
+			}
+			if(count>0) {
+				flag=true;
+				break;
+			}
+			i++;
 		}
-			System.out.println(sum);
-		
+		if(flag==true) {
+			System.out.println("not a prime");
+		}
+		else {
+			System.out.println("prime");
+		}
 	}
 
 }
