0d7837fd4e3b8be67376eb8333d79ffcadd5c00f
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-12 08:20:47 +0530

    switch case

--- src/Bharath/Vaj.java
+++ src/Bharath/Vaj.java
@@ -6,23 +6,19 @@ public class Vaj {
 
 	
 	public static void main(String[] args) {
-		int i,m=0, a=0;
-		int n = 7;
-		m = n/2;
-		if(n==0 || n==1) {
-			System.out.println(n +  "is not a prime number");}
-		else {
-			for(i=2;i<=m;i++) {
-				if (n%i==0) {
-					System.out.println(n +  "is not a prime number");
-					a=1;
-				}
-			}
-		}
-		if(a==0) {
-			System.out.println(n +  "is a prime number");
-		}
+		int day = 5;
 		
+		switch (day) {
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			System.out.println("weekdays");
+			break;
+		case 6:
+		case 7:
+			System.out.println("weekend");}
 			
 		
 		
---
+++ src/Bharath/prime.java
@@ -0,0 +1,22 @@
+package Bharath;
+
+public class prime {
+public static void main(String args[]) {
+	int num=100,count;
+	int i,j;
+	for( i=2;i<=7;i++) {
+		count=0;
+	for( j=1;j<=i;j++) {// 1<=1
+	if(i%j==0) {
+		count++;
+		
+	}
+	}
+	if(count==2) {
+		System.out.println(i);
+	}
+	}
+	
+	
+}
+}
