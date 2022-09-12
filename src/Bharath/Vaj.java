192fd5068d75b50e7e59059ad85dbda74d174ea8
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-12 08:53:37 +0530

    Bing Bang

--- src/Bharath/Vaj.java
+++ src/Bharath/Vaj.java
@@ -7,8 +7,17 @@ public class Vaj {
 	
 	public static void main(String[] args) {
 		int i = 1;
-		while (i<15) {
+		while (i<20) {
+			if (i%3==0 && i%5==0) {
+				System.out.println("Bing Bang");
+			}
 			if (i%3==0) {
+				System.out.println("Bing");
+			}
+			else if (i%5==0) {
+				System.out.println("Bang");
+			}
+			else if((i%3!=0) && (i%5!=0)) {
 				System.out.println(i);
 			}
 			i++;
