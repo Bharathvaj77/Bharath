95134fdb28bf17e0d6cd14d7b9859b406c72607e
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-15 09:32:37 +0530

    Decimal to Binary

---
+++ src/Bharath/Addingnumbers.java
@@ -0,0 +1,14 @@
+package Bharath;
+
+public class Addingnumbers {
+
+	public static void main(String[] args) {
+		int a=1;
+		for(int i=1;i<=10;i++) {
+			System.out.println(a);
+			a =a+i;
+		}
+
+	}
+
+}
---
+++ src/Bharath/decimaltobinary.java
@@ -0,0 +1,28 @@
+package Bharath;
+
+public class decimaltobinary {
+
+	public static void main(String[] args) {
+		int a=20, i=0;
+		int b[] =new int[10]; 
+		while(a>0) {
+			b[i]=a%2;
+		System.out.println(b[i]);
+			a=a/2;
+			i++;
+
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+	for(int j=i-1;j>=0;j--) {
+		System.out.println(b[j]);
+	}
+
+}
+}
