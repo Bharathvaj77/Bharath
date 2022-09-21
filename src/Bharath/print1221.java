3523b37c3634245bfee8ce3103a05a246788ea33
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-21 09:47:54 +0530

    print 1221

---
+++ src/Bharath/print1221.java
@@ -0,0 +1,21 @@
+package Bharath;
+
+public class print1221 {
+
+	public static void main(String[] args) {
+		int n=4;
+		int i,j;
+		for( i=1;i<=3;i++) {
+			 for(j=1;j<=n;j++)
+				if(j==1 || j==n) {
+					System.out.print("1");
+				}
+				else {
+					System.out.print("2");
+				}
+			 System.out.println();
+		}
+		
+	}
+
+}
