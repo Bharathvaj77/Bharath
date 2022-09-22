9ef89c47d7771db70972ebe656aaf0866625531e
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-22 19:40:37 +0530

    printing odd & even

--- src/Bharath/Vaj.java
+++ src/Bharath/Vaj.java
@@ -8,8 +8,12 @@ public class Vaj {
 	public static void main(String[] args) {
 		//int i=1;
 		for(int i=1;i<7;i++) {
-			System.out.println(i+(i-1));
+			System.out.print(i+(i-1));
 			
+		}
+		System.out.println(" ");
+		for(int i=1;i<7;i++) {
+			System.out.print(i*2);
 		}
 	
 		
