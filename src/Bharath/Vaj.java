4605aadc4d47fd18570f3aef0da27347be4f8a5b
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-22 19:56:10 +0530

    multiply by 12345......

--- src/Bharath/Vaj.java
+++ src/Bharath/Vaj.java
@@ -6,11 +6,10 @@ public class Vaj {
 
 	
 	public static void main(String[] args) {
-		int num=1;
-		for(int i=1;i<3;i++) {
-		num=num*11;
 		
-				System.out.println(num);
+		for(int i=1;i<10;i++) {
+			System.out.println(i*i);
+			
 		}
 	
 		
