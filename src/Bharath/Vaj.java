b8e4f84f29e365f8a2801a425e99509da769a627
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-22 20:04:12 +0530

    cubic program

--- src/Bharath/Vaj.java
+++ src/Bharath/Vaj.java
@@ -6,9 +6,9 @@ public class Vaj {
 
 	
 	public static void main(String[] args) {
-		
-		for(int i=1;i<10;i++) {
-			System.out.println(i*i);
+		int n=5;
+		for(int i=1;i<n;i++) {
+			System.out.println(i*(i*i));
 			
 		}
 	
