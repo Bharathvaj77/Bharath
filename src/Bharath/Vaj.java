30af5fb0412c8a0bfd2cd992f7432ebdc9b3da78
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-22 19:37:22 +0530

    printing odd nums

--- src/Bharath/Vaj.java
+++ src/Bharath/Vaj.java
@@ -6,9 +6,10 @@ public class Vaj {
 
 	
 	public static void main(String[] args) {
-		
-		for(int i=1; i<=5 ;i++) {
-			System.out.println(i);
+		//int i=1;
+		for(int i=1;i<7;i++) {
+			System.out.println(i+(i-1));
+			
 		}
 	
 		
