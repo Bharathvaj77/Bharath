5d972b17a00c10ddb714354b5a5418b46d1ff267
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-22 19:26:21 +0530

    printing 2 table

--- src/Bharath/Vaj.java
+++ src/Bharath/Vaj.java
@@ -7,11 +7,8 @@ public class Vaj {
 	
 	public static void main(String[] args) {
 		
-		for(int i=0; i<20 ;i++) {
-			if(i%2!=0) {
-				continue;
-			}
-			System.out.println(i);
+		for(int i=0; i<10 ;i++) {
+			System.out.println(i*2);
 		}
 	
 		
