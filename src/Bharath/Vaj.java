06bac9d1f63a02cad40cd42fb785d63950b55711
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-23 11:23:39 +0530

    printing maximum number and its position

--- src/Bharath/Vaj.java
+++ src/Bharath/Vaj.java
@@ -7,35 +7,20 @@ public class Vaj {
 	
 	public static void main(String[] args) {
 		int a[]= {3,4,6,8,10,12};
-		int b[]=new int[6];
-		
-		
-		//int max=a[0];
-		//int index=0;
-		//int max2=a[1];
-		
-		for(int i=0;i<a.length;i++) 
+		int max=a[0];
+		int pos=0;
+		for(int i=0;i<a.length;i++)
 		{
-			b[i]=a[i];
-			System.out.println(b[i]);
+			if(a[i]>max)
+			{
+				max=a[i];
+				pos=i;
+				
+			}
+			//System.out.print(max);
+			//System.out.print(pos);
 		}
-			//if(a[i]>max)
-		//	{
-				//max=a[i];
-				//index=i;
-		//	}
-			
-	//	}
-		//for(int j=0;j<a.length;j++) {
-			//if(max2!=max)
-		//}
-	//System.out.println(b[i]);
-	
-	
-	
-		
-		
-		
-		
+		System.out.println(max);
+		System.out.println(pos);
 	}
 }
