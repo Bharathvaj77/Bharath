ee6f17045d2edb37ebdbfdc714c1adc372327612
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-23 11:57:24 +0530

    find the second maximum number in array

--- src/Bharath/Vaj.java
+++ src/Bharath/Vaj.java
@@ -6,9 +6,11 @@ public class Vaj {
 
 	
 	public static void main(String[] args) {
-		int a[]= {3,4,6,8,10,12};
+		int a[]= {210,90,7,100};
 		int max=a[0];
+		int max2=a[0];
 		int pos=0;
+		int pos2=0;
 		for(int i=0;i<a.length;i++)
 		{
 			if(a[i]>max)
@@ -17,10 +19,19 @@ public class Vaj {
 				pos=i;
 				
 			}
-			//System.out.print(max);
-			//System.out.print(pos);
 		}
-		System.out.println(max);
-		System.out.println(pos);
+		System.out.println("The maximum number is:" + max);
+		System.out.println("The position of maximum number is:" + pos);
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]!=max) 
+			{
+				max2=a[i];
+				pos2=i;
+			
+			}
+		}
+		System.out.println("The second maximum number is:" + max2);
+		System.out.println("The position of second maximum number is:" + pos2);
 	}
 }
