dd6d6b66bb88f859dcc35b5089e5116b8dbd1e80
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-23 20:41:27 +0530

    finding first 20 prime number

--- src/Bharath/prime.java
+++ src/Bharath/prime.java
@@ -2,9 +2,9 @@ package Bharath;
 
 public class prime {
 public static void main(String args[]) {
-	int num=100,count;
+	int num=10,count;
 	int i,j;
-	for( i=2;i<=7;i++) {
+	for( i=2;i<=20;i++) {
 		count=0;
 	for( j=1;j<=i;j++) {// 1<=1
 	if(i%j==0) {
