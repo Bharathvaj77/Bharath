864e60403f70d57ba40b6f8797aeefb32eca1322
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-27 19:52:32 +0530

    fibonacci series with 3 variables

--- src/Bharath/fibonacciseries.java
+++ src/Bharath/fibonacciseries.java
@@ -6,14 +6,13 @@ public class fibonacciseries {
 		// TODO Auto-generated method stub
 		int n1=-1;
 		int n2=1;
-		for(int i=0;i<=7;i++) {
-			System.out.println(n1+n2);
-			n2=n1+n2;
-			n1=n2-n1;
-			
-			
+		int n3; 
+		for(int i=0;i<7;i++) {
+			n3=n1+n2;
+			n1=n2;
+			n2=n3;
+			System.out.println(n3);			
 		}
-
 	}
 
 }
