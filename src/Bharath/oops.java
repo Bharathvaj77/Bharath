28bf2e550e9e8d70888759ac0ccb78b92a27bf7c
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-27 12:37:43 +0530

    oops class & object

--- src/Bharath/oops.java
+++ src/Bharath/oops.java
@@ -1,12 +1,19 @@
 package Bharath;
 
 public class oops {
-	{
-		int a=10;
-	}
-	public static void main(String[] args) {
+			int a;
+			void area() 
+			{
+			System.out.println(a*a);
+		}
 		
-
+	public static void main(String[] args) {
+		oops s1=new oops();
+		oops s2=new oops();
+		s1.a=5;
+		s2.a=10;
+		s1.area();
+		s2.area();
 	}
 
 }
