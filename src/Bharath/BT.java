e9933b06563666312554c212c70a60d752bc6a5b
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-27 12:54:07 +0530

    printing 1*2 2*3 3*4 4*5 5*6

--- src/Bharath/BT.java
+++ src/Bharath/BT.java
@@ -3,7 +3,14 @@ package Bharath;
 public class BT {
 
 	public static void main(String[] args) {
-		
+		int a=1;
+		int b=2;
+		while(a<=5 && b<=6) {
+			int c=a*b;
+			System.out.println(c);
+			++a;
+			++b;
+		}
 
 	}
 }
