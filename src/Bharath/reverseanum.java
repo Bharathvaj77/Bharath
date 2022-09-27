e5ad1a8917ee0154dd86c568dc63a6b3c4ca0d98
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-27 20:08:22 +0530

    reverse a number

---
+++ src/Bharath/reverseanum.java
@@ -0,0 +1,16 @@
+package Bharath;
+
+public class reverseanum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=9876,rev=0;
+		while(num!=0) {
+			int r=num%10;
+			rev=rev*10+r;
+			num=num/10;
+		}
+		System.out.println(rev);
+	}
+
+}
---
+++ src/Bharath/squarecube.java
@@ -0,0 +1,13 @@
+package Bharath;
+
+public class squarecube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		for(int i=1;i<n;i++) {
+			System.out.println(i*(i*i));
+
+		}
+	}
+	}
