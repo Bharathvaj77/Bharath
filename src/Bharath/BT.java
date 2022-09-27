7acbc63cf239e690f4b7e8b8b5c6f78b172bba11
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-27 12:47:23 +0530

    finding prime number

--- src/Bharath/BT.java
+++ src/Bharath/BT.java
@@ -3,17 +3,7 @@ package Bharath;
 public class BT {
 
 	public static void main(String[] args) {
-		for(int i=1;i<30;i++) {
-			if(i%3==0 && i%5==0) {
-				System.out.print(i+" ");
-			}
-		}
-		//System.out.println(" ");
-		//for(int i=1;i<50;i++) {
-			//if(i%5==0) {
-				//System.out.print(i+" ");
-			//}
-		//}
+		
 
 	}
 }
