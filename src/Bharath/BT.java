cc5351f34160041fd93df60f4e6ceeda9405e7d4
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-27 12:45:02 +0530

    printing only multiples of 3 & 5

--- src/Bharath/BT.java
+++ src/Bharath/BT.java
@@ -4,16 +4,16 @@ public class BT {
 
 	public static void main(String[] args) {
 		for(int i=1;i<30;i++) {
-			if(i%3==0) {
+			if(i%3==0 && i%5==0) {
 				System.out.print(i+" ");
 			}
 		}
-		System.out.println(" ");
-		for(int i=1;i<50;i++) {
-			if(i%5==0) {
-				System.out.print(i+" ");
-			}
-		}
+		//System.out.println(" ");
+		//for(int i=1;i<50;i++) {
+			//if(i%5==0) {
+				//System.out.print(i+" ");
+			//}
+		//}
 
 	}
 }
