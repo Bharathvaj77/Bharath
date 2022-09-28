e154da5c429c40a4c41cace7147715631ebe9b5f
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-28 19:09:27 +0530

    palindrome

--- src/Bharath/reverseanum.java
+++ src/Bharath/reverseanum.java
@@ -4,7 +4,7 @@ public class reverseanum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num=987654321,rev=0;
+		int num=1221,rev=0;
 		while(num!=0) {
 			int r=num%10;
 			rev=rev*10+r;
