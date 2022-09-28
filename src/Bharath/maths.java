1b2d0653db763ce43f01bd69bfe37d66f0d86993
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-09-28 09:40:08 +0530

    factorial for 5 & 6 in class & object

--- src/Bharath/maths.java
+++ src/Bharath/maths.java
@@ -1,18 +1,20 @@
 package Bharath;
 
 public class maths {
-	int sum;
+	int sum=1 ,n;
 	void factorial() {
-	for(int i=1;i<=5;i++) {
+	for(int i=1;i<=n;i++) {
 		sum=sum*i;
-		System.out.println(sum);
 	}
+		System.out.println(sum);
 	}
 	public static void main(String[] args) {
 		maths fac=new maths();
-		fac.sum=1;
+		fac.n=5;
 		fac.factorial();
-	
+		maths fac1=new maths();
+		fac1.n=6;
+		fac1.factorial();
 
 }
 }
