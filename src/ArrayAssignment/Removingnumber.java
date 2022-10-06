0a60aae63efacc67cf83e55a07609cd9eeac20a9
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-10-06 10:12:05 +0530

    Removing a given number

---
+++ src/ArrayAssignment/Removingnumber.java
@@ -0,0 +1,23 @@
+package ArrayAssignment;
+
+public class Removingnumber {
+	void remove(int a[]) {
+		int n=4;
+		int i=0;
+		
+		for( i=0;i<a.length;i++) {
+			if(a[i]==n) {
+				continue;
+			}
+				System.out.println(a[i]);
+			
+		}
+		
+	}
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4,5};
+		Removingnumber rn=new Removingnumber();
+		rn.remove(a);
+	}
+
+}
