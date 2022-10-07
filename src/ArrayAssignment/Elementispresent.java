8e4dcd56beb3ed1da8cef3502d2a4f04cb552b23
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-10-07 22:49:20 +0530

    How many times a given element is present

--- src/ArrayAssignment/Elementispresent.java
+++ src/ArrayAssignment/Elementispresent.java
@@ -1,18 +1,18 @@
 package ArrayAssignment;
-
+        //8.How many times a given element is present
 public class Elementispresent {
 	void element(int a[]) {
-		//int b=0;
+		int arr=a[0];
 		int pos=0;
 		for(int i=0;i<a.length;i++) {
-			if(a[i]==pos) {
+			if(a[i]==arr) {
 				pos++;
 			}
 		}
-		
+		System.out.println(pos);
 	}
 	public static void main(String[] args) {
-		int a[]= {1,2,3,4,2,5,2};
+		int a[]= {1,2,3,4,5,1,2,2,1};
 		Elementispresent ep=new Elementispresent();
 		ep.element(a);
 
