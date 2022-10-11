15f55765ad6d6d1cd7e2392cae514113fccc3c7e
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-10-11 11:52:18 +0530

    placing a given number in last index of a array

--- src/ArrayAssignment/placingnumber.java
+++ src/ArrayAssignment/placingnumber.java
@@ -1,17 +1,18 @@
 package ArrayAssignment;
-
+    //7.placing a given number in last index of a array
 public class placingnumber {
 	void place(int a[]) {
-		int n=6;
-		for(int i=0;i<=n+1;i++) {
-			if(a[i]==n+1) {
-				
+		int store=66;
+		int pos=a[4];
+		for(int i=0;i<a.length;i++) {
+			if(a[i]==pos) {
+			 System.out.println(store);
 			}
 			System.out.println(a[i]);
 		}
 	}
 	public static void main(String[] args) {
-		int a[]= {1,2,3,4,5};
+		int a[]= {10,20,30,44,55};
 		placingnumber pn =new placingnumber();
 		pn.place(a);
 
