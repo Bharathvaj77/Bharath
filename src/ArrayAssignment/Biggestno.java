b4f96e55f547da995660137350fcddff44212059
bharathvaj <bharathvaj@bharathvaj-HP-Laptop-15s-fq2xxx>
2022-10-11 11:50:01 +0530

    Finding first two biggest no in a given array

--- src/ArrayAssignment/Biggestno.java
+++ src/ArrayAssignment/Biggestno.java
@@ -1,14 +1,23 @@
 package ArrayAssignment;
-   //9.Finding Biggest no in given Array
+   //9,11.Finding  first two Biggest no in given Array
 public class Biggestno {
 	void largest(int a[]) {
 		int max=0;
-		for(int i=0;i<a.length;i++) {
-			if(a[i]>max) {
+		int max2=0;
+		for(int i=0;i<a.length;i++) 
+		{
+			if(a[i]>max) 
+			{
+				max2=max;
 				max=a[i];
+			}
+			if(max!=a[i] && a[i]>max2) 
+			{
+				max2=a[i];
 			}
 		}
 		System.out.println("The Biggest no in given Array is "  +  max);
+		System.out.println("The second biggest no in a given array is " + max2);
 	}
 	public static void main(String[] args) {
 		int a[]= {10,22,12,43,37};
